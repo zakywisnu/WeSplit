89d3d2f5086de9ef412efc110e7800ea6b61dab9
Ahmad Zaky W <azakyw@gmail.com>
2021-03-07 18:54:37 +0700

    Initial Commit

---
+++ WeSplitApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplitApp
+//
+//  Created by Ahmad Zaky on 07/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Ahmad Zaky on 07/03/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
